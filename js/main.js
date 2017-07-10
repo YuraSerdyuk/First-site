773058902073a9e887101e6586fcff536dc006bb
Yura Serdyuk <yurko7203@gmail.com>
2017-07-11 00:00:20 +0300

    first commit

---
+++ js/main.js
@@ -0,0 +1,37 @@
+jQuery(document).ready(function() {
+
+  jQuery(".sl").slick({
+        prevArrow: '<img class="slick-prev" src="img/shape-left.png">',
+          nextArrow: '<img class="slick-next" src="img/shape-right.png">',
+          dots: true,
+
+
+  });
+})
+
+
+
+
+jQuery(document).ready(function() {
+
+  jQuery(".sl2").slick({
+        prevArrow: '<img class="slick-prev2" src="img/shape-left.png">',
+        nextArrow: '<img class="slick-next2" src="img/shape-right.png">',
+
+
+
+  });
+})
+
+
+
+jQuery(document).ready(function() {
+
+  jQuery(".sl3").slick({
+        prevArrow: '<img class="slick-prev3" src="img/shape-left.png">',
+        nextArrow: '<img class="slick-next3" src="img/shape-right.png">',
+
+
+
+  });
+})
